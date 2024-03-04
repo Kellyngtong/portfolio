2abf285c5844c41ccf22003671264ea9f7954183
Kellyntong <aitor_paterna@hotmail.com>
2024-03-04 21:25:42 +0000

    componet navbar created

---
+++ src/components/navbar/navbar.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Mi Componente</h1>
+      <p>Este es mi componente creado en Next.js</p>
+    </div>
+  );
+};
+
+export default Navbar;
