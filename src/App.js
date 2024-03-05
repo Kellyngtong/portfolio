3a57b7e082fe02c370a597d355e45bc5a67fa6a0
Kellyntong <aitor_paterna@hotmail.com>
2024-03-05 00:43:31 +0000

    reate new function open an embebed website and new info panels

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/navbar.component.tsx";
 import Panel from "./components/panel/panel.component.tsx";
+import PanelOne from "./components/views/panels/panelOne/panelOne.component.tsx";
+import PanelPersonal from "./components/views/panels/panelPersonal/panelPersonal.component.tsx";
 
 function App() {
   const [actualPanel, setActualPanel] = useState(0);
+
+  const [personalPanel, setPersonalPanel] = useState(true);
 
   useEffect(() => {}, [actualPanel]);
 
@@ -13,15 +17,8 @@ function App() {
       <Navbar element="hola" setter={setActualPanel} getter={actualPanel} />
       <section className="app-section">
         {actualPanel === 0 && (
-          <Panel customClass="">
-            <button
-              onClick={() => {
-                setActualPanel(actualPanel + 1);
-              }}
-            >
-              primera prueba {actualPanel}
-            </button>
-            <div>lorem ipsum</div>
+          <Panel customClass="panel-PanelOne">
+            <PanelOne setPersonalPanel={setPersonalPanel} />
           </Panel>
         )}
         {actualPanel === 1 && (
@@ -60,9 +57,24 @@ function App() {
             <div>lorem ipsum</div>
           </Panel>
         )}
-        <Panel customClass="rightPanel">
-          <div>lorem ipsum</div>
-        </Panel>
+        {personalPanel ? (
+          <Panel customClass="rightPanel personal-panel">
+            <PanelPersonal actualPanel={actualPanel} />
+          </Panel>
+        ) : (
+          <Panel customClass="rightPanel">
+            <header className="header-qraneos">
+              <div
+                onClick={() => {
+                  setPersonalPanel(true);
+                }}
+              >
+                X
+              </div>
+            </header>
+            <iframe src="https://qraneos.com/"></iframe>
+          </Panel>
+        )}
       </section>
     </div>
   );
---
+++ src/components/views/panels/panelOne/panelOne.component.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./panelOne.css";
+
+interface PanelOneProps {
+  setPersonalPanel: (boolean) => void;
+}
+
+const PanelOne = ({ setPersonalPanel }: PanelOneProps) => {
+  return (
+    <>
+      <article>
+        <span>Hello,</span>
+        <span>Aitor here</span>
+      </article>
+      <span>I made this portfolio</span>
+      <span>To</span>
+      <span>
+        Apply on{" "}
+        <b
+          onClick={() => {
+            setPersonalPanel(false);
+          }}
+        >
+          Qraneos. S.L.
+        </b>
+      </span>
+      <span>Enjoy it!</span>
+    </>
+  );
+};
+
+export default PanelOne;
